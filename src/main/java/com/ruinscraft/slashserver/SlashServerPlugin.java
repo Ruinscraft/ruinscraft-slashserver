8e09201ff712ebd6a97cb685b65133506b7fae93
Dexton Anderson <contact@dextonanderson.com>
2020-04-09 22:56:15 -0500

    use Command, reflection to add to bukkit command map

--- src/main/java/com/ruinscraft/slashserver/SlashServerPlugin.java
+++ src/main/java/com/ruinscraft/slashserver/SlashServerPlugin.java
@@ -3,10 +3,14 @@ package com.ruinscraft.slashserver;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Field;
 import java.util.Set;
 
 public class SlashServerPlugin extends JavaPlugin {
@@ -17,33 +21,47 @@ public class SlashServerPlugin extends JavaPlugin {
 
         Set<String> commands = getConfig().getConfigurationSection("commands").getKeys(false);
 
-        for (String command : commands) {
-            String server = getConfig().getString("commands." + command + ".server");
-            String permission = getConfig().getString("commands." + command + ".permission");
+        for (String label : commands) {
+            String server = getConfig().getString("commands." + label + ".server");
+            String permission = getConfig().getString("commands." + label + ".permission");
 
-            CommandExecutor commandExecutor = (sender, command1, label, args) -> {
-                if (!(sender instanceof Player)) {
-                    return false;
-                }
-
-                Player player = (Player) sender;
-
-                if (permission != null && !permission.isEmpty()) {
-                    if (!player.hasPermission(permission)) {
-                        player.sendMessage(ChatColor.RED + "You do not have permission for this server.");
+            Command command = new Command(label) {
+                @Override
+                public boolean execute(CommandSender sender, String label, String[] args) {
+                    if (!(sender instanceof Player)) {
                         return false;
                     }
+
+                    Player player = (Player) sender;
+
+                    if (permission != null && !permission.isEmpty()) {
+                        if (!player.hasPermission(permission)) {
+                            player.sendMessage(ChatColor.RED + "You do not have permission for this server.");
+                            return false;
+                        }
+                    }
+
+                    sendToServer(player, server);
+
+                    return true;
                 }
-
-                sendToServer(player, server);
-
-                return true;
             };
 
-            getCommand(command).setExecutor(commandExecutor);
+            registerCommand(command);
         }
 
         getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+    }
+
+    private void registerCommand(Command command) {
+        try {
+            Field bukkitCommandMap = getServer().getClass().getDeclaredField("commandMap");
+            bukkitCommandMap.setAccessible(true);
+            CommandMap commandMap = (CommandMap) bukkitCommandMap.get(getServer());
+            commandMap.register("ruinscraft-slashserver", command);
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     private void sendToServer(Player player, String server) {
